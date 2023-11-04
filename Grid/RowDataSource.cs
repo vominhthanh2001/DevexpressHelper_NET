31dd37243a551844dddc9c09b54c3159a6c8c8cc
ThanhDev2001 <vominhthanhunl@gmail.com>
2023-11-04 10:40:11 +0700

    Update ForeColor & BackColor

--- Grid/RowDataSource.cs
+++ Grid/RowDataSource.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using DevExpress.Office.Import.OpenXml;
 
 namespace DevexpressHelper_NET.Grid
 {
@@ -17,13 +19,19 @@ namespace DevexpressHelper_NET.Grid
 
         public RowDataSource(GridControl gridControl, GridView gridView) : base(gridControl, gridView)
         {
-
+            gridView.RowCellStyle += GridView_RowCellStyle;
         }
 
         public RowDataSource(GridControl gridControl, GridView gridView, List<TModel> models) : base(gridControl, gridView, models)
         {
-
+            gridView.RowCellStyle += GridView_RowCellStyle;
         }
+
+        private object _lockSetColor = new object();
+        private Color _color = default(Color);
+        private int _index = -1;
+        private bool _foreColor = false;
+        private bool _backColor = false;
 
         private int[] IndexsRowSelected()
         {
@@ -58,6 +66,63 @@ namespace DevexpressHelper_NET.Grid
             var rows = indexs.Select(x => base.Models[x]).Cast<TModel>().ToList();
 
             return rows;
+        }
+
+        public void SetForeColorCell()
+        {
+            GridView.Columns[""].AppearanceCell.ForeColor = Color.White;
+        }
+
+        public void SetForeColorRow(TModel model, Color color)
+        {
+            int index = Models.IndexOf(model);
+
+            if (index == -1)
+                return;
+
+            _index = index;
+            _color = color;
+            _foreColor = true;
+        }
+
+        public void SetBackColorRow(TModel model, Color color)
+        {
+            int index = Models.IndexOf(model);
+
+            if (index == -1)
+                return;
+
+            _index = index;
+            _color = color;
+            _backColor = true;
+        }
+
+        private void GridView_RowCellStyle(object sender, RowCellStyleEventArgs e)
+        {
+            lock (_lockSetColor)
+            {
+                int index = e.RowHandle;
+
+                if (index == -1)
+                    return;
+
+                if (_index == -1 && _color == default(Color))
+                    return;
+
+                if (_index == index && _color != default(Color) && _foreColor)
+                {
+                    e.Appearance.ForeColor = _color;
+                }
+                else if (_index == index && _color != default(Color) && _backColor)
+                {
+                    e.Appearance.BackColor = _color;
+                }
+
+                _index = -1;
+                _color = default(Color);
+                _foreColor = false;
+                _backColor = false;
+            }
         }
     }
 }
